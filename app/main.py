5db9906ab5a92443c646ae477419e3ee8bf69831
wariiipana <wariipana1154@gmail.com>
2025-11-30 08:54:24 -0500

    Adding Package File

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from app.database.database import SessionLocal, engine
 import app.database.models.medition
-import app.crud
-
+from app.crud import crear_medicion
 from app.schemas import MedicionCreate
 
 
 app.database.models.medition.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
+
 
 def get_db():
     db = SessionLocal()
@@ -20,7 +29,7 @@ def get_db():
 
 @app.post("/api/mediciones")
 def recibir_medicion(data: MedicionCreate, db: Session = Depends(get_db)):
-    nueva = app.crud.crear_medicion(db, data)
+    nueva = crear_medicion(db, data)
     return {"status": "ok", "id": nueva.id}
 
 @app.get("/api/mediciones")
