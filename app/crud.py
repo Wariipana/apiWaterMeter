630a7b431152e8396549676ea2131b9d4d264e8b
wariiipana <wariipana1154@gmail.com>
2025-11-30 08:42:15 -0500

    Adding Package File

--- app/crud.py
+++ app/crud.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Session
 from . import schemas
-from database.models.medition import Medicion
+from database.models import medition
 
 def crear_medicion(db: Session, data: schemas.MedicionCreate):
-    medicion = Medicion(**data.dict())
+    medicion = medition.Medicion(**data.dict())
     db.add(medicion)
     db.commit()
     db.refresh(medicion)
     return medicion
 
 def obtener_mediciones(db: Session, limit=1000):
-    return db.query(Medicion).order_by(Medicion.id.desc()).limit(limit).all()
+    return db.query(medition.Medicion).order_by(medition.Medicion.id.desc()).limit(limit).all()
